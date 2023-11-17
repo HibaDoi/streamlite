a6b4b1cbde200e27cd4eab8e17b9a167bd86fe2a
hiba doi <doihiba@iav.ac.ma>
2023-11-18 00:43:33 +0100

    hey

---
+++ essai.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import pydeck as pdk
+import geopandas as gpd
+#on ajoute notre fichier geoparquet
+datafile ='waaaa.geoparquet'
+#on lit notre fichier geoparquet
+data = pd.read_parquet(datafile)
+#on ajoute un titre a notre page
+st.header("station de meteo")
+#add raw data 
+if st.checkbox('show raw data'):
+    st.subheader('raw data')
+    st.write(data)
+#on affiche la map
+#st.map(data)
+#on affiche avec symbologie
+# Ici on a trouver une erreur dans laquel on a transformer depuis pandas to geopandas avant utiliser 
+geometry = gpd.points_from_xy(data['longitude'], data['latitude'])
+gdf = gpd.GeoDataFrame(data, geometry=geometry)
+#Ici on essai de faire les symbole proportionnel 
+#initial view  position  , zoom et inclinaison ça sera benifique en 3D 
+"""st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=32,
+        longitude=-5,
+        zoom=11,
+        pitch=0,
+    ),
+    #Ici on peut ajouter une infinitée de couche et ler symbology 
+    layers=[
+        pdk.Layer(
+        'ScatterplotLayer',
+        data=gdf,
+        pickable=True,
+        opacity=0.8,
+        stroked=True,
+        filled=True,
+        radius_scale=6,
+        radius_min_pixels=1,
+        radius_max_pixels=100,
+        line_width_min_pixels=1,
+        get_position='[longitude, latitude]',
+        get_radius="temp_j3",
+        get_fill_color=[255, 140, 0],
+        get_line_color=[0, 0, 0],
+        ),
+        
+    ],
+))"""
+
+view = pdk.View(type="_GlobeView", controller=True, width=1000, height=700)
+st.pydeck_chart(pdk.Deck(
+    views=[view],
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=32,
+        longitude=-5,
+        zoom=5,
+        pitch=0,
+    ),
+    parameters={"cull": True},
+    #Ici on peut ajouter une infinitée de couche et ler symbology 
+    layers=[
+    pdk.Layer(
+        "ColumnLayer",
+        id="temp_j3",
+        data=gdf,
+        get_elevation="temp_j3",
+        get_position=["longitude", "latitude"],
+        elevation_scale=1000,
+        pickable=True,
+        auto_highlight=True,
+        radius=2000,
+        get_fill_color='[255, 165, 0]',
+        ),
+        
+    ],
+))
