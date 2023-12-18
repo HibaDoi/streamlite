d236ca1477ae09c322bc3ae5bd2054e240ddc5f1
HibaDoi <102768127+HibaDoi@users.noreply.github.com>
2023-12-18 22:07:34 +0100

    Update 7_🌻_Filter AL.py

--- pages/7_🌻_Filter AL.py
+++ pages/7_🌻_Filter AL.py
@@ -49,9 +49,9 @@ def cho(value,cool):
     return slider_range
 def bm(genre):
     if genre=="LIGHT":
-        y=pdk.map_styles.LIGHT
+        y=pdk.map_styles.CARTO_LIGHT_NO_LABELS
     elif genre=="DARK":
-        y=pdk.map_styles.DARK
+        y=pdk.map_styles.CARTO_DARK_NO_LABELS
     elif genre=="SATELLITE":
         y=pdk.map_styles.SATELLITE
     elif genre=="ROAD":
